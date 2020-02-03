e7fbde8f329fbe2224b4bd592e946a6d1eed9409
Raina Shin <yewon95@vt.edu>
2020-02-03 13:56:50 -0500

    working on App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
 
 import Header from './components/Header'
-import Book from './components/Book';
+import BookList from './components/BookList';
 import Search from './components/Search'
 
 class App extends Component {
   
+  state = {
+    books: [],
+    printFilter: '',
+    q: ''
+  }
 
 
+  handleSubmit = ( event, searchInput ) => {
+    event.preventDefault();
+
+    this.setState({
+      q: searchInput
+    });
+    const baseUrl = 'https://www.googleapis.com/books/v1/volumes'
+    const key = 'AIzaSyDVrJeQ2nTuO5MM9DDITJun2b5ZpjP26aI';
+    const url = this.queryURL(baseUrl, searchInput, key);
+    
+
+    fetch(url)
+      .then(response => {
+        if(!response.ok) {
+          throw new Error('Something went wrong, please try again later');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Good response From Google Books API: ', data)
+        this.setState({
+          books: data,
+          error: null
+        });
+      })
+      .catch(error => {
+        this.setState({
+          error: error.message
+        });
+      });
+  }
+c
 
   render() {
     return (
@@ -15,8 +52,8 @@ class App extends Component {
         <Header />
 
         <Search />
-        
-        <Book />
+
+        <BookList books={this.state.books}/>
       </div>
     );
   }
--- src/components/Book.js
+++ src/components/Book.js
@@ -2,22 +2,15 @@ import React from 'react'
 
 
 function Book(props) {
-    let title;
-    let author;
-    let imgURL;
-    let description;
-    let cost;
-
-
+    
     return (
         <div className='book'>
             <li className='book-list'>
-                <img src= { imgURL } className='book-imgURL' alt={`${title }`}></img>
+                <img src= {props.book.volumeInfo.imageLinks.thumbnail} className='book-imgURL' alt='thumbnail'></img>
                 <div className= 'book-details'>
-                    <h2 className='book-title'> { title} </h2>
-                    <p className='book-author'> { author } </p>
-                    <p className='book-description'> { description } </p>
-                    <p className='book-cost'> { cost } </p>
+                    <h2 className='book-title'> {props.book.volumeInfo.title} </h2>
+                    <p className='book-author'> {props.book.volumeInfo.author } </p>
+                    <p className='book-description'> {props.book.volumeInfo.description} </p>
                 </div>
             </li>
         </div>
